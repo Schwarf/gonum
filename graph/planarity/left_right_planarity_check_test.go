dd7b3e2e132816f176eacaafdd38992485d877af
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-06-09 14:34:57 +0200

    Add tests with binary trees and wheel graphs.

--- graph/planarity/left_right_planarity_check_test.go
+++ graph/planarity/left_right_planarity_check_test.go
@@ -52,6 +52,49 @@ func starGraph(numNodes int64) graph.Undirected {
 	return g
 }
 
+// binaryTreeGraph returns a binary tree of numNodes vertices (0-based heap order).
+func binaryTreeGraph(numNodes int64) graph.Undirected {
+	g := simple.NewUndirectedGraph()
+	for i := int64(0); i < numNodes; i++ {
+		g.AddNode(simple.Node(i))
+	}
+	for i := int64(0); i < numNodes; i++ {
+		left := 2*i + 1
+		right := 2*i + 2
+		if left < numNodes {
+			g.SetEdge(g.NewEdge(simple.Node(i), simple.Node(left)))
+		}
+		if right < numNodes {
+			g.SetEdge(g.NewEdge(simple.Node(i), simple.Node(right)))
+		}
+	}
+	return g
+}
+
+// wheelGraph returns a wheel graph on numNodes vertices (center at 0).
+func wheelGraph(numNodes int64) graph.Undirected {
+	if numNodes < 4 {
+		panic("wheelGraph requires at least 4 nodes")
+	}
+	g := simple.NewUndirectedGraph()
+	for i := int64(0); i < numNodes; i++ {
+		g.AddNode(simple.Node(i))
+	}
+	// cycle on 1..numNodes-1
+	for i := int64(1); i < numNodes; i++ {
+		next := i + 1
+		if next == numNodes {
+			next = 1
+		}
+		g.SetEdge(g.NewEdge(simple.Node(i), simple.Node(next)))
+	}
+	// spokes from center 0
+	for i := int64(1); i < numNodes; i++ {
+		g.SetEdge(g.NewEdge(simple.Node(0), simple.Node(i)))
+	}
+	return g
+}
+
 func TestPlanarEmptyGraph(t *testing.T) {
 	g := simple.NewUndirectedGraph()
 	if !IsPlanar(g) {
@@ -90,6 +133,24 @@ func TestPlanarStarGraphs(t *testing.T) {
 		g := starGraph(n)
 		if !IsPlanar(g) {
 			t.Errorf("Star graph of size %d should be planar", n)
+		}
+	}
+}
+
+func TestPlanarTreeGraphs(t *testing.T) {
+	for n := int64(2); n <= maxNumberOfNodes; n++ {
+		g := binaryTreeGraph(n)
+		if !IsPlanar(g) {
+			t.Errorf("Binary tree graph of size %d should be planar", n)
+		}
+	}
+}
+
+func TestPlanarWheelGraphs(t *testing.T) {
+	for n := int64(4); n <= maxNumberOfNodes; n++ {
+		g := wheelGraph(n)
+		if !IsPlanar(g) {
+			t.Errorf("Wheel graph of size %d should be planar", n)
 		}
 	}
 }
